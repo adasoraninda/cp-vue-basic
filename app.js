89f9ab522af63b349692b21d1ddda6f98b781f4e
adasoraninda <adasoraninda@gmail.com>
2021-08-13 14:58:24 +0700

    Add section 1-6

---
+++ app.js
@@ -0,0 +1,50 @@
+var dataProducts = {
+  maximum: 50,
+  products: [
+    {
+      id: "1",
+      name: "One pice 1",
+      description: "Jaket",
+      price: "150",
+      image:
+        "https://s2.bukalapak.com/img/273942026/large/JAKET_ONE_PIECE_LUFFY.jpg",
+    },
+    {
+      id: "2",
+      name: "One pice 2",
+      description: "Jaket",
+      price: "10",
+      image:
+        "https://s2.bukalapak.com/img/273942026/large/JAKET_ONE_PIECE_LUFFY.jpg",
+    },
+    {
+      id: "3",
+      name: "One pice 3",
+      description: "Jaket",
+      price: "10",
+      image:
+        "https://s2.bukalapak.com/img/273942026/large/JAKET_ONE_PIECE_LUFFY.jpg",
+    },
+    {
+      id: "4",
+      name: "One pice 4",
+      description: "Jaket",
+      price: "150",
+      image:
+        "https://s2.bukalapak.com/img/273942026/large/JAKET_ONE_PIECE_LUFFY.jpg",
+    },
+    {
+      id: "5",
+      name: "One pice 5",
+      description: "Jaket",
+      price: "150",
+      image:
+        "https://s2.bukalapak.com/img/273942026/large/JAKET_ONE_PIECE_LUFFY.jpg",
+    },
+  ],
+};
+
+var app = new Vue({
+  el: "#app",
+  data: dataProducts,
+});
